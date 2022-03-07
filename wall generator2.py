ffe047357fe847cffca108c97825e6006f78c427
Artur Babinski <46361319+quenrythane@users.noreply.github.com>
2022-03-07 09:29:09 +0100

    little variable name changes and try to make file more clear

--- wall generator2.py
+++ wall generator2.py
@@ -1,13 +1,14 @@
 brick = '[--]'
 in_layer = 5
-whole = 12
+bricks_ammount = 12
 
 
-def prepare_layers(whole, in_layer):
+# prepare list where every element is part of wall (1 element = 1 layer)
+def prepare_layers(bricks_ammount, in_layer):
     layers = ['']  # start with first layer
     last_layer = 0
     for i in range(whole):
-        x = i // in_layer  # 0
+        x = i // in_layer 
         if x > last_layer:  # check when i need to add next layer to wall
             last_layer = x
             layers.append(brick)
@@ -16,6 +17,7 @@ def prepare_layers(whole, in_layer):
     return layers[::-1]
 
 
+# take list from above and print the wall picture
 def build_the_wall(layers):
     for i, layer in enumerate(layers):
         if i % 2:
@@ -26,20 +28,15 @@ def build_the_wall(layers):
         else:
             print(layer)
             
-            
-def build_the_whole_wall(whole, in_layer):
-    layers = prepare_layers(whole, in_layer)
-    for i, layer in enumerate(layers):
-        if i % 2:
-            if len(layer) == len(brick) * in_layer:
-                print(layer[2:] + layer[:2])
-            else:
-                print(' ' * (len(brick) // 2) + layer)
-        else:
-            print(layer)
 
 
-layers = prepare_layers(whole, in_layer)
+# it's sum of 2 function above            
+def build_the_whole_wall(bricks_ammount, in_layer):
+    layers = prepare_layers(bricks_ammount, in_layer)
+    build_the_wall(layers)
+
+
+layers = prepare_layers(bricks_ammount, in_layer)
 build_the_wall(layers)
 print()
 build_the_whole_wall(12, 5)
